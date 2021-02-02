45c3f178c45190cb5bb8cd186b38f9d6259a1277
frankhn <harfrank2@gmail.com>
2021-02-02 07:49:20 +0200

    implements camera QRCode scanner

--- main.py
+++ main.py
@@ -2,18 +2,28 @@ import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
 
-img = cv2.imread('assets/qr2.png')
+# img = cv2.imread('assets/qr2.png')
 
-for barcode in decode(img):
-    print(barcode.data)
-    myData = barcode.data.decode('utf-8')
-    print(myData)
-    pts = np.array([barcode.polygon], np.int32)
-    pts = pts.reshape((-1, 1, 2))
-    cv2.polylines(img, [pts], True, (255, 0, 255), 5)
-    pts2 = barcode.rect
-    cv2.putText(img, myData, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,
-                0.9, (255, 0, 255), 2)
+# Video capture
+cap = cv2.VideoCapture(0)
+# width
+cap.set(3, 640)
+# Height
+cap.set(4, 480)
 
-cv2.imshow('Result', img)
-cv2.waitKey(0)
+while True:
+    success, img = cap.read()
+    for barcode in decode(img):
+        print(barcode.data)
+        myData = barcode.data.decode('utf-8')
+        print(myData)
+        pts = np.array([barcode.polygon], np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(img, [pts], True, (255, 0, 255), 5)
+        pts2 = barcode.rect
+        cv2.putText(img, myData, (pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9, (255, 0, 255), 2)
+
+    cv2.imshow('Result', img)
+    # For video capture you put 1
+    cv2.waitKey(1)
